f5f87e29e6f50033fe6fe328b10cdbd492ffdebb
jarograv <jgraveley@hotmail.com>
2017-10-05 21:25:31 -0400

    Add files via upload

---
+++ Fortbattle-Export.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Fort Battle to CSV Exporter
+// @description  Exports battles from the-west to csv spreadsheet format
+// @version      0.1
+// @author       jarograv
+// @match           https://*.the-west.net/game.php*
+// @match           https://*.the-west.de/game.php*
+// @match           https://*.the-west.pl/game.php*
+// @match           https://*.the-west.nl/game.php*
+// @match           https://*.the-west.se/game.php*
+// @match           https://*.the-west.ro/game.php*
+// @match           https://*.the-west.com.pt/game.php*
+// @match           https://*.the-west.cz/game.php*
+// @match           https://*.the-west.es/game.php*
+// @match           https://*.the-west.ru/game.php*
+// @match           https://*.the-west.com.br/game.php*
+// @match           https://*.the-west.org/game.php*
+// @match           https://*.the-west.hu/game.php*
+// @match           https://*.the-west.gr/game.php*
+// @match           https://*.the-west.dk/game.php*
+// @match           https://*.the-west.sk/game.php*
+// @match           https://*.the-west.fr/game.php*
+// @match           https://*.the-west.it/game.php*
+// @downloadURL     
+// @updateURL       
+// @grant        none
+// ==/UserScript==

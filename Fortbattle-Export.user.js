5bc04c786ef46518637f9a502cf8aa33a8cd2443
jarograv <jgraveley@hotmail.com>
2017-10-05 21:58:45 -0400

    Add files via upload

--- Fortbattle-Export.user.js
+++ Fortbattle-Export.user.js
@@ -21,7 +21,28 @@
 // @match           https://*.the-west.sk/game.php*
 // @match           https://*.the-west.fr/game.php*
 // @match           https://*.the-west.it/game.php*
-// @downloadURL     
-// @updateURL       
-// @grant        none
+// @downloadURL     https://raw.githubusercontent.com/jarograv/Fortbattle-Export/master/Fortbattle-Export.user.js
+// @updateURL       https://raw.githubusercontent.com/jarograv/Fortbattle-Export/master/Fortbattle-Export.user.js
+// @grant        	none
+// @run-at          document-end
 // ==/UserScript==
+var fortbattleImport = {
+	VersionControl: {
+		version: 0.1,
+		isOutdated: function() {
+			return fortbattleImport.localData.latestVersion > fortbattleImport.VersionControl.version;
+		},
+		notifyOutdated: function() {
+			if (fortbattleImport.VersionControl.isOutdated()) {
+				new west.gui.Dialog('Fortbattle-Export is outdated', 'There\'s a new version of available. Do you want to install it?', west.gui.Dialog.SYS_WARNING).addButton("Install!", function() {
+					window.open('https://raw.githubusercontent.com/jarograv/Fortbattle-Export/master/Fortbattle-Export.user.js', '_blank');
+				}).addButton("Close", function() {}).show();
+			}
+		}
+	},
+	localData:{
+		init: function() {
+		
+		}
+	}
+}
